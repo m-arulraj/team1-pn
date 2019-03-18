0215e6f96071532b532cd8be1add794d1d433ff4
mukul <mukul.utrani@gmail.com>
2019-03-18 20:08:43 +0530

    base

---
+++ professionalnetworking/src/main/java/com/virtusa/professionalnetworking/domain/SkillServices.java
@@ -0,0 +1,6 @@
+package com.virtusa.professionalnetworking.domain;
+
+
+public class SkillServices {
+
+}
---
+++ professionalnetworking/src/main/java/com/virtusa/professionalnetworking/repository/SkillRepository.java
@@ -0,0 +1,9 @@
+package com.virtusa.professionalnetworking.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.virtusa.professionalnetworking.domain.Skill;
+
+public interface SkillRepository extends JpaRepository<Skill, Long> {
+
+}
