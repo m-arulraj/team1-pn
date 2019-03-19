4c1a241c1f5186b7b6be6843b14c786ccb4849c1
mukul <mukul.utrani@gmail.com>
2019-03-19 14:38:32 +0530

    created schema

--- registrationapi/src/main/java/com/virtusa/registrationapi/RegistrationapiApplication.java
+++ registrationapi/src/main/java/com/virtusa/registrationapi/RegistrationapiApplication.java
@@ -1,8 +1,12 @@
 package com.virtusa.registrationapi;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class RegistrationapiApplication {
 
 	public static void main(String[] a){
-		
+		SpringApplication.run(RegistrationapiApplication.class, a);
 	}
 }
