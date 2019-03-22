d181e635d30803e9c3ef11bd025b43e825d0fa94
mukul <mukul.utrani@gmail.com>
2019-03-22 11:10:19 +0530

    project and skill updated

---
+++ registrationapi/src/main/java/com/virtusa/registrationapi/repository/ProjectRegistrationRepository.java
@@ -0,0 +1,15 @@
+package com.virtusa.registrationapi.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.virtusa.registrationapi.domain.Project;
+
+
+@Repository
+public interface ProjectRegistrationRepository extends JpaRepository<Project, Long> {
+
+	public Optional<Project> findByType(String name);
+}
